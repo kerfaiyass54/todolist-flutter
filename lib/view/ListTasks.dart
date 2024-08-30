36b0bdbc433b3b10acdce3734b0262afb2e240ed
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-08-30 22:08:16 +0100

    Created Tasks list view file

---
+++ lib/view/ListTasks.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListTasks extends StatefulWidget {
+  const ListTasks({super.key});
+
+  @override
+  State<ListTasks> createState() => _ListTasksState();
+}
+
+class _ListTasksState extends State<ListTasks> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
