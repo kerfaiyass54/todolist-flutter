f052c74be2451c510e079bc4c3fd5a2c200a8d92
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-08-30 21:58:53 +0100

    created model and provider

---
+++ lib/model/Task.dart
@@ -0,0 +1,7 @@
+class Task{
+  String name;
+  bool finished;
+  String description;
+  DateTime dateOfFinish;
+  Task(this.name,this.finished,this.description,this.dateOfFinish);
+}
---
+++ lib/provider/TaskModel.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list/model/Task.dart';
+
+class CartModel extends ChangeNotifier {
+  /// Internal, private state of the cart.
+  final List<Task> _todotasks = [];
+
+  /// An unmodifiable view of the items in the cart.
+  List<Task> get todotasks => _todotasks;
+
+
+}
